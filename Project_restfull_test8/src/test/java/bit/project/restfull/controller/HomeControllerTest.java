c490a5d595dd5fad7bf2ab17a4da48c73344b818
github-jj08 <hooray3915@naver.com>
2020-10-26 09:25:40 +0900

    풀 하기전 머지 & 커밋

---
+++ Project_restfull_test8/src/test/java/bit/project/restfull/controller/HomeControllerTest.java
@@ -0,0 +1,89 @@
+package bit.project.restfull.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIndex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAjaxSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContent_view() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLikeCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAjaxNoticeList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotice_view() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFAQ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPersonal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHomeController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHomeControllerBoardServiceAdminBoardService() {
+		fail("Not yet implemented");
+	}
+
+}
